a24a0a5524f2a6a7a174ff88d093921b25908eb1
J K <kj688895@gmail.com>
2020-09-16 01:15:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,10 +7,10 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep 5
+    sleep 5.seconds
     number -= 1
   end
   return "HAPPY NEW YEAR!"
